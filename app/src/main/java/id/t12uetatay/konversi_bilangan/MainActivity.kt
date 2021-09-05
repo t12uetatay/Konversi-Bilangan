431b2c0e1a5a9ae800cb17548cdb9f0714aa5f3d
t12ue <t12uetatay@gmail.com>
2021-09-05 23:46:47 +0700

    Initial project

---
+++ app/src/main/java/id/t12uetatay/konversi_bilangan/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.t12uetatay.konversi_bilangan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
